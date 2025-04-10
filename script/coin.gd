802e0fa88f69f63cc11b37fce208e3203abd1d7d
TestAccountDADA <spam@aljoschaboersch.de>
2025-04-10 16:35:40 +0200

    Cleanup

--- script/coin.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var gamemanager: Node = %Gamemanager
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-
-func _on_body_entered(body: Node2D) -> void:
-	gamemanager.add_point()
-	animation_player.play("pickup animation")
--- script/game_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var score = 0
-@onready var score_lable: Label = $ScoreLable
-
-func add_point():
-	score += 1
-	score_lable.text = "You Collected " + str(score) + " coins!"
-	
